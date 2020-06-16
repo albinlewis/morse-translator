97de9492b42b6894fd8a775ad3f6a3219a8cfab2
albin geraud <albingeraud@gmail.com>
2020-06-16 17:05:05 +0200

    add encode and decode methods for translate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
 import { Component } from '@angular/core';
+import { morseEncode, morseDecode } from './morse-coding';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'morse-translator';
+  public isDecoding = false;
+  public inputText = '';
+  public translatedText = '';
+
+  switchMode() {
+    this.isDecoding = !this.isDecoding;
+  }
+
+  setInputLabel() {
+    if (this.isDecoding) {
+      return 'Enter text to decode';
+    } else {
+      return 'Enter text to encode';
+    }
+  }
+
+  setTitle() {
+    if (this.isDecoding) {
+      return 'Morse Decoder';
+    } else {
+      return 'Morse Encoder';
+    }
+  }
+
+  setSwitchText() {
+    if (this.isDecoding) {
+      return 'Switch to encoding mode';
+    } else {
+      return 'Switch to decoding mode';
+    }
+  }
+
+  morseCode(textToEncode: string) {
+    let encodedText = '';
+    for (const char of textToEncode) {
+      if (char !== ' ') {
+        const encodedChar = morseEncode(char) ? morseEncode(char) : '';
+        encodedText += encodedChar + ' ';
+      } else {
+        encodedText += '    ';
+      }
+    }
+    return encodedText;
+  }
+
+  morseDecode(textToDecode: string) {
+    textToDecode += ' ';
+    let decodedText = '';
+    let morseChar = '';
+    let i = 0;
+    for (const char of textToDecode) {
+      if (char !== ' ') {
+        i = 0;
+        morseChar += char;
+      } else {
+        i += 1;
+
+        if (i === 2) {
+          decodedText += ' ';
+        } else {
+          decodedText += morseDecode(morseChar) ? morseDecode(morseChar) : '';
+          morseChar = '';
+        }
+      }
+    }
+    return decodedText;
+  }
 }
